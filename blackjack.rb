ca79c7ef1eeabc7bbd9fb7f9abba60a9b873a704
garth0323 <garth0323@gmail.com>
2014-01-12 23:48:22 +0000

    class version

--- blackjack.rb
+++ blackjack.rb
@@ -1,151 +1,136 @@
+require 'rubygems'
+require 'pry'
+
+class Card
+  attr_accessor(:suit, :face_value)
+  
+  def initialize(s, fv)
+    @suit = s
+    @face_value = fv
+  end
+  
+  def pretty_output
+    puts "The #{@face_value} of #{suit}"
+  end
+  
+  def to_s
+    pretty_output
+  end
+  
+  def find_suit
+    ret_val = case suit
+                when 'H' then 'Hearts'
+                when 'D' then 'Diamonds'
+                when 'S' then 'Spades'
+                when 'C' then 'Clubs'
+               end
+    ret_val
+  end
+end
+
 class Deck
+  attr_accessor(:cards)
   
-  @@deck = []
-  
-  def initialize(name)
-    @game = name  
-  end
-  
-  #returns 6 decks shuffled
-  def new_deck
-    suits = ['H', 'D', 'S', 'C']
-    cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    
-    #uses six decks
-    @@deck = suits.product(cards)
-    5.times { @@deck << suits.product(cards) } 
-  end
-  
-  def deal_card
-    @@deck.pop
-  end
-  
-  def shuffle_deck
-    @@deck.shuffle!
-  end
-
-end
-
-module GamePlay
-  def compare(dealertotal, playertotal)
-    if dealertotal > playertotal
-      puts "Sorry, dealer wins."
-    elsif dealertotal < playertotal
-      puts "Congratulations, you win!"
-    else
-      puts "It's a tie!"
+  def initialize
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
     end
+    scramble!
+  end
+  
+  def scramble!
+    cards.shuffle!
+  end
+  
+  def deal_one
+    cards.pop
+  end
+  
+  def size
+    cards.size
   end
 end
-
-class Player
-  include GamePlay
-  
-  @@hand = []
-  @@total = 0
-  
-  def initialize(name)
-    @name = name
-  end
-  
-  def hand
-    @@hand
+                  
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do |card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
   end
   
   def total
-    @@total
-  end
-  
-  def first_deal(card_1, card_2)
-    @@hand << card_1
-    @@hand << card_2
-    puts "#{@name} has #{card_1} and #{card_2}"
-  end
-  
-  def calculate_total(cards) 
-    arr = cards.map{|e| e[1] }
-  
+    face_values = cards.map{|card| card.face_value}
+    
     total = 0
-    arr.each do |value|
-      if value == "A"
+    face_values.each do |val|
+      if val == "A"
         total += 11
-      elsif value.to_i == 0 # J, Q, K
-        total += 10
       else
-        total += value.to_i
+        total += (val.to_i == 0 ? 10 : val.to_i)
       end
     end
-  
-    #correct for Aces
-    arr.select{|e| e == "A"}.count.times do
-      total -= 10 if total > 21
+    
+    #correct for aces
+    face_values.select{|val| val == "A"}.count.times do
+      break if total <= 21
+      total -= 10
     end
     
     total
   end
   
-  def stand(total)
-    "#{@name} stands with #{total}."
+  def add_card(new_card)
+    cards << new_card
   end
   
-  def hit(card)
-    @@hand << card
-    "#{@name} hits, card is #{card} you now have #{calculate_total(hand)}"
+  def is_busted?
+    total > 21
+  end
+end
+
+                  
+class Player
+  include Hand
+  
+  attr_accessor(:name,:cards)
+  
+  def initialize(n)
+    @name = n
+    @cards = []
   end
   
 end
 
-class Dealer < Player
-  include GamePlay
+class Dealer
+  include Hand
   
-  @@delhand = []
-  @@total = 0
+  attr_accessor(:name,:cards)
   
-  def initialize(name)
-    @name = name
-  end
-  
-  def first_deal(card_1, card_2)
-    @@delhand << card_1
-    @@delhand << card_2
-    puts "#{@name} is showing #{card_1}."
-  end
-  
-  def hit_or_stand(dealertotal)
-    if dealertotal == 21
-      puts "Sorry, dealer hit blackjack. You lose."
-      exit
-    end
-
-    while dealertotal < 17
-      #hit
-      new_card = game_cards.deal_card
-      puts "Dealing new card for dealer: #{new_card}"
-      @@delhand << new_card
-      dealertotal = dealer.calculate_total(dealer.hand)
-      puts "Dealer total is now: #{dealertotal}"
-
-      if dealertotal > 21
-        puts "Congratulations, dealer busted! You win!"
-        exit
-      else 
-        #compare using GamePlay module
-        compare(dealer.calculate_total(dealer.hand),player_1.calculate_total(player_1.hand))
-      end
-    end
-  end
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end 
 end
 
 
-game_cards = Deck.new('game_one')
-game_cards.new_deck
-game_cards.shuffle_deck
-player_1 = Player.new("Garth")
-player_1.first_deal(game_cards.deal_card, game_cards.deal_card)
-playertotal = player_1.calculate_total(player_1.hand)
-puts player_1.stand(playertotal)
-#puts player_1.hit(game_cards.deal_card)
-dealer = Dealer.new("Dealer")
-dealer.first_deal(game_cards.deal_card, game_cards.deal_card)
-dealertotal = dealer.calculate_total(dealer.hand)
-puts dealertotal
+deck = Deck.new
+
+player = Player.new("Garth")
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.show_hand
+
+dealer = Dealer.new
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.show_hand
+
